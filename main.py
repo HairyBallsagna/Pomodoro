78415c34e292ab3b666c9a757ab4b084dfa044f4
Lakshay <lakshayyadav2006@gmail.com>
2021-12-07 12:13:38 +0530

    Got the work sessions and break sessions for the timer working.

--- main.py
+++ main.py
@@ -7,25 +7,45 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1 #25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    
+    work_seconds = WORK_MIN * 60
+    short_break_seconds = SHORT_BREAK_MIN * 60
+    long_break_seconds = LONG_BREAK_MIN * 60
+    
+    if reps % 8 == 0:
+        count_down(long_break_seconds)
+    elif reps % 2 == 0:
+        count_down(short_break_seconds)
+    else:
+        count_down(work_seconds)
+        
+        
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    if count < 0: 
-        return
-    
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    if count_min < 10:
+        count_min = f"0{count_min}"
+    
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
-    window.after(1000, count_down, count - 1)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
