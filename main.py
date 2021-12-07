47905a5b966d96f75040b41b05682dd3663ef28a
Lakshay <lakshayyadav2006@gmail.com>
2021-12-07 12:43:57 +0530

    Reformatted the file.

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from tkinter import *
 import math
 
-# ---------------------------- CONSTANTS ------------------------------- #
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1 #25
-SHORT_BREAK_MIN = 2 #5
-LONG_BREAK_MIN = 5 #20
+WORK_MIN = 5
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 reps = 0
 timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
 def reset_timer():
     window.after_cancel(timer)
@@ -23,7 +22,7 @@ def reset_timer():
     global reps
     reps = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
 def start_timer():
     global reps
     reps += 1
@@ -43,13 +42,13 @@ def start_timer():
         title_label.config(text="Work", fg=GREEN)
         
         
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
     if count_sec < 10:
         count_sec = f"0{count_sec}"
+    if count_min < 10:
+        count_min = f"0{count_min}"
     
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
@@ -61,7 +60,8 @@ def count_down(count):
         for _ in range(math.floor(reps / 2)):
             marks += "✔"
         check_marks.config(text=marks)
-# ---------------------------- UI SETUP ------------------------------- #
+        
+        
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
